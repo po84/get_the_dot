d128149ac244b65e26ffce6ebba3970e7f1b4830
Po Tong <potong616@gmail.com>
2019-09-19 22:51:19 -0700

    I think this can be called a game, jaja

--- Player.gd
+++ Player.gd
@@ -1,5 +1,10 @@
 extends KinematicBody2D
 
+const FLOOR_NORMAL = Vector2(0, -1)
+const SPEED = 350
+const JUMP_HEIGHT = -500
+const GRAVITY = 15
+
 var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
@@ -9,13 +14,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	velocity.x = 0
-	velocity.y += 10
+	velocity.y += GRAVITY
+
+	if is_on_floor():
+		velocity.y = 0
 	
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 200
+		velocity.x = SPEED
 	if Input.is_action_pressed("ui_left"):
-		velocity.x = -200
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -200
+		velocity.x = -SPEED
+	if Input.is_action_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_HEIGHT
 		
-	move_and_slide(velocity)
+	move_and_slide(velocity, FLOOR_NORMAL)
+	
+func reset_velocity():
+	velocity = Vector2(0, 0)
